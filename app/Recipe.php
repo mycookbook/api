4caf6da61ce0647f556394839dd581043ad7b5fe
andela-fokosun <okosunuzflorence@gmail.com>
2017-09-02 02:39:45 +0100

    :tada: modify user to recipe relationship

--- app/Recipe.php
+++ app/Recipe.php
@@ -25,7 +25,7 @@ class Recipe extends Model
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function users()
+    public function user()
     {
         return $this->belongsTo('App\User');
     }
