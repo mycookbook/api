b7fe19f39ffccfbb41931882d3bdfe3e81159ea6
andela-fokosun <okosunuzflorence@gmail.com>
2017-10-01 12:46:02 +0300

    configs

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SearchController
+ *
+ * @author Florence Okosun <florence.okosun@getitllc.com>
+ * @copyright Copyright(c) 2017 Get It, LLC(http://www.getit.me)
+ */
+
+namespace App\Http\Controllers;
+
+
+class SearchController
+{
+
+}
---
+++ config/cors.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * ${NAME}
+ *
+ * @author Florence Okosun <florence.okosun@getitllc.com>
+ * @copyright Copyright(c) 2017 Get It, LLC(http://www.getit.me)
+ */
