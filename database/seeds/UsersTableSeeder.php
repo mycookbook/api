f5e8f3fa9a56984555e7cdba7353677e4390f9e6
andela-fokosun <okosunuzflorence@gmail.com>
2017-10-17 18:54:53 +0300

    code that was once lost is found found glory

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,12 +1,54 @@
 <?php
 
-/**
- * UserTableSeeder
- *
- * @author Florence Okosun <florence.okosun@getitllc.com>
- * @copyright Copyright(c) 2017 Get It, LLC(http://www.getit.me)
- */
-class UserTableSeeder
-{
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
+/**
+ * Class Users Table Seeder
+ */
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')
+            ->insert(
+                [
+                    'name' => str_random(10),
+                    'email' => str_random(10).'@gmail.com',
+                    'password' => app('hash')->make('secret'),
+                    'following' => 0,
+                    'followers' => 0,
+                    'created_at' => date("Y-m-d H:i:s"),
+                    'updated_at' => date("Y-m-d H:i:s")
+                ]
+            );
+        DB::table('cookbooks')
+            ->insert(
+                [
+                    'name' => str_random(10),
+                    'description' => str_random(50),
+                    'user_id' => 1,
+                    'created_at' => date("Y-m-d H:i:s"),
+                    'updated_at' => date("Y-m-d H:i:s")
+                ]
+            );
+        DB::table('recipes')
+            ->insert(
+                [
+                    'name' => str_random(10),
+                    'description' => str_random(50),
+                    'ingredients' => str_random(8),
+                    'imgUrl' => 'http://via.placeholder.com/350x150',
+                    'user_id' => 1,
+                    'cookbook_id' => 1,
+                    'created_at' => date("Y-m-d H:i:s"),
+                    'updated_at' => date("Y-m-d H:i:s")
+                ]
+            );
+    }
 }
