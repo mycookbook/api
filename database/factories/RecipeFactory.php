2c82d3aa55176009a0a44f68ab69544b97e116c0
andela-fokosun <okosunuzflorence@gmail.com>
2017-11-02 18:34:48 +0300

    implement recipe factory

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * ${NAME}
- *
- * @author Florence Okosun <florence.okosun@getitllc.com>
- * @copyright Copyright(c) 2017 Get It, LLC(http://www.getit.me)
- */
+
+$factory->define(
+    App\Recipe::class, function (Faker\Generator $faker) {
+        return [
+            'name' => $faker->streetName,
+            'ingredients' => 'simple recipe',
+            'description' => $faker->sentence(),
+            'imgUrl' => $faker->imageUrl(800, 600, 'food'),
+            'user_id' => rand(2, 49),
+            'cookbook_id' => rand(3, 45),
+        ];
+    }
+);
