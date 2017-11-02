0652e8301669b0f8da89f35805f6e1f76baf23be
andela-fokosun <okosunuzflorence@gmail.com>
2017-11-02 23:28:33 +0300

    :wrench: config

--- app/support/helpers.php
+++ app/support/helpers.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * ${NAME}
- *
- * @author Florence Okosun <florence.okosun@getitllc.com>
- * @copyright Copyright(c) 2017 Get It, LLC(http://www.getit.me)
- */
+
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
+}
