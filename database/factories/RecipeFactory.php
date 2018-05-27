5792ef17acd1fabdc9d894afd9fac29c6815aaaa
andela-fokosun <okosunuzflorence@gmail.com>
2018-05-28 00:49:54 +0300

    chore

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -7,8 +7,8 @@ $factory->define(
             'ingredients' => 'simple recipe',
             'description' => $faker->sentence(),
             'imgUrl' => $faker->imageUrl(800, 600, 'food'),
-            'user_id' => rand(2, 49),
-            'cookbook_id' => rand(3, 45),
+            'user_id' => rand(1, 4),
+            'cookbook_id' => rand(1, 4),
         ];
     }
 );
