37f649bb90e8caa16cf920b4a579b5225f6a6367
fokosun <okosunuzflorence@gmail.com>
2018-07-04 17:31:56 +0100

    unit test slugify helper function

---
+++ tests/Helpers/HelpersTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * HelpersTest
+ */
+class HelpersTest extends TestCase
+{
+
+    public function testSlugifyFunction()
+    {
+        $output = slugify("plain text");
+
+        $this->assertEquals("plain-text", $output);
+    }
+}
