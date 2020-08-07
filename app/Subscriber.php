c38bb6fb4a9ca9e337c1eba36217eeb5ecdc58ef
fokosun <okosunuzflorence@gmail.com>
2020-08-07 00:10:21 -0400

    add subscribers migrations

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Suscriber extends Model
+class Subscriber extends Model
 {
 	protected $fillable = ['email'];
 }
---
+++ database/migrations/2020_08_07_015216_create_subscriber_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->json('subscriptions')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
