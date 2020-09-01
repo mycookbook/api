13f54f9cdfe83e5a1528fc1d062aa404c4825dd1
fokosun <okosunuzflorence@gmail.com>
2020-08-31 23:36:46 -0600

    add fulltext indexes to tables add resource type field

---
+++ database/migrations/2020_09_01_042354_add_fulltext_index_to_cookbooks_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddFulltextIndexToCookbooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		DB::statement('ALTER TABLE cookbooks ADD FULLTEXT full(name, description)');
+    }
+}
