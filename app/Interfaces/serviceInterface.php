bd68ecb64d7f20ffe2b9b2853541956494016b46
fokosun <okosunuzflorence@gmail.com>
2020-09-07 20:19:46 -0600

    implement user contact detail update

--- app/Interfaces/serviceInterface.php
+++ app/Interfaces/serviceInterface.php
@@ -9,6 +9,6 @@ interface serviceInterface
 	public function index();
 	public function show($option);
 	public function store(Request $request);
-	public function update(Request $request, $option);
+	public function update(Request $request, string $option);
 	function get($q);
 }
