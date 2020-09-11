0339d4793680c213ad5b2e4208b9034405ad1ef8
fokosun <okosunuzflorence@gmail.com>
2020-09-11 00:10:53 -0600

    WIP

---
+++ app/CategoryCookbook.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryCookbook extends Model
+{
+	protected $table = 'category_cookbook';
+
+	public function cookbooks()
+	{
+		return $this->belongsToMany('App\Cookbook');
+	}
+}
