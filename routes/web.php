8dffdf9ea9372a07951cfd63eee3bdb87ccf4321
fokosun <okosunuzflorence@gmail.com>
2020-09-26 11:58:12 -0600

    organize routes

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,36 @@
  * @link     Somelink <http://somelink.com>
  */
 
+use Illuminate\Support\Facades\Route;
+
 $router->group(
     ['prefix' => 'api/v1'], function () use ($router) {
+		/*
+		|--------------------------------------------------------------------------
+		| PUBLIC ROUTES
+		|--------------------------------------------------------------------------
+		*/
+
         $router->get(
             '/', function () {
                 return 'Cookbook API v1.0';
             }
         );
 
+		/*
+		|--------------------------------------------------------------------------
+		| Search
+		|--------------------------------------------------------------------------
+		*/
         $router->get(
         	'/search', 'SearchController@fetch'
 		);
 
+		/*
+		|--------------------------------------------------------------------------
+		| Users Auth
+		|--------------------------------------------------------------------------
+		*/
         $router->post(
             '/auth/register', 'UserController@store'
         );
@@ -40,13 +58,67 @@ $router->group(
             '/users/{username}', 'UserController@show'
         );
 
+		/*
+		|--------------------------------------------------------------------------
+		| Statistics
+		|--------------------------------------------------------------------------
+		*/
         $router->get(
             '/stats/', 'StatsController@index'
         );
 
-        // Developers
+		/*
+		|--------------------------------------------------------------------------
+		| Cookbooks
+		|--------------------------------------------------------------------------
+		*/
+		$router->get('/cookbooks', 'CookbookController@index');
+		$router->get('/cookbooks/{cookbookId}', 'CookbookController@show');
+
+		/*
+		|--------------------------------------------------------------------------
+		| Recipes
+		|--------------------------------------------------------------------------
+		*/
+		$router->get('/recipes', 'RecipeController@index');
+		$router->get('/recipes/{recipeId}', 'RecipeController@show');
+		$router->post('add-clap', 'RecipeController@addClap');
+
+		/*
+		|--------------------------------------------------------------------------
+		| Subscriptions
+		|--------------------------------------------------------------------------
+		*/
+		$router->post('subscriptions', 'SubscriptionController@store');
+
+		/*
+		|--------------------------------------------------------------------------
+		| Categories and Definitions
+		|--------------------------------------------------------------------------
+		*/
+
+		$router->get('/categories', 'CategoryController@index'); //get all categories
+		$router->get('/definitions', 'DefinitionsController@index'); //get all definitions
+
+
+		/**
+		 * Email verification
+		 */
+		$router->get('verify-email/{token}', 'UserController@verifyEmail');
+		$router->get('resend-email-verification-link/{token}', 'UserController@resend');
+
+		/*
+		|--------------------------------------------------------------------------
+		| PROTECTED ROUTES
+		|--------------------------------------------------------------------------
+		*/
         $router->group(
-            ['middleware' => 'jwt.auth'], function () use ($router) {
+            [
+            	'middleware' => [
+            		'must-verify-email',
+					'jwt.auth'
+				]
+			], function () use ($router) {
                 $router->put(
                     '/users/{username}', 'UserController@update'
                 );
@@ -54,63 +126,29 @@ $router->group(
                 $router->patch(
                     '/users/{username}', 'UserController@update'
                 );
+
+				/*
+				|--------------------------------------------------------------------------
+				| Recipes Routes
+				|--------------------------------------------------------------------------
+				*/
+				$router->post('/recipes', 'RecipeController@store');
+
+				$router->put('/recipes/{recipeId}', 'RecipeController@update')
+					->patch('/recipes/{recipeId}', 'RecipeController@update');
+
+				$router->delete('/recipes/{recipeId}', 'RecipeController@delete');
+
+				/*
+				|--------------------------------------------------------------------------
+				| Cookbooks Routes
+				|--------------------------------------------------------------------------
+				*/
+
+				$router->post('/cookbooks', 'CookbookController@store'); //create new cookbook
+				$router->put('/cookbooks/{cookbookId}', 'CookbookController@update'); //update one cookbook
+				$router->delete('/cookbooks/{cookbookId}', 'CookbookController@delete'); //delete one cookbook
             }
         );
-
-        /*
-        |--------------------------------------------------------------------------
-        | Recipes Routes
-        |--------------------------------------------------------------------------
-        */
-
-        $router->get('/recipes', 'RecipeController@index')
-            ->post('/recipes', 'RecipeController@store');
-
-        $router->put('/recipes/{recipeId}', 'RecipeController@update')
-            ->patch('/recipes/{recipeId}', 'RecipeController@update')
-            ->get('/recipes/{recipeId}', 'RecipeController@show');
-
-        $router->delete('/recipes/{recipeId}', 'RecipeController@delete');
-
-        /*
-        |--------------------------------------------------------------------------
-        | Cookbooks Routes
-        |--------------------------------------------------------------------------
-        */
-
-        $router->get('/cookbooks', 'CookbookController@index'); //get all cookbooks
-        $router->post('/cookbooks', 'CookbookController@store'); //create new cookbook
-        $router->get('/cookbooks/{cookbookId}', 'CookbookController@show');
-        $router->put('/cookbooks/{cookbookId}', 'CookbookController@update'); //update one cookbook
-        $router->delete('/cookbooks/{cookbookId}', 'CookbookController@delete'); //delete one cookbook
-
-		/*
-		|--------------------------------------------------------------------------
-		| Subscription Routes
-		|--------------------------------------------------------------------------
-		*/
-		$router->post('subscriptions', 'SubscriptionController@store');
-
-
-		/*
-		|--------------------------------------------------------------------------
-		| Categories Routes
-		|--------------------------------------------------------------------------
-		*/
-
-		$router->get('/categories', 'CategoryController@index'); //get all categories
-		$router->get('/definitions', 'DefinitionsController@index'); //get all definitions
-
-		/*
-		* recipes claps
-		*/
-
-		$router->post('add-clap', 'RecipeController@addClap'); //increment recipe clap
-
-		/**
-		 * Email verification
-		 */
-		$router->get('verify-email/{token}', 'UserController@verifyEmail');
-		$router->get('resend-email-verification-link/{token}', 'UserController@resend');
     }
 );
