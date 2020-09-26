888e7248249a5d959bb0beff65f92b6398ae8e06
fokosun <okosunuzflorence@gmail.com>
2020-09-26 00:13:20 -0600

    better seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -106,7 +106,7 @@ class DatabaseSeeder extends Seeder
 				'pronouns' => $pronouns[rand(0,2)],
 				'expertise_level' => $levels[$rand_level],
 				'can_take_orders' => array_rand($can_take_orders),
-				'about' => 'Rrow itself, let it be sorrow; let him love it; let him pursue it, ishing for its acquisitiendum. Because he will ab hold, uniess but through concer, and also of those who resist. Now a pure snore disturbeded sum dust. He ejjnoyes, in order that somewon, also with a severe one, unless of life. May a cusstums offficer somewon nothing of a poison-filled. Until, from a twho, twho chaffinch may also pursue it, not even a lump. But as twho, as a tank; a proverb, yeast; or else they tinscribe nor. Yet yet dewlap bed. Twho may be, let him love fellows of a polecat. Now amour, the, twhose being, drunk, yet twhitch and, an enclosed valley’s always a laugh. In acquisitiendum the Furies are Earth; in (he takes up) a lump vehicles bien',
+				'about' => $faker->paragraph(6),
 				'email_verified' => '2020-01-01 00:00:00'
 			]);
 
