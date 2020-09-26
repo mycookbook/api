900b8d0e014b41078d8150c9758f93c63f0a41ae
fokosun <okosunuzflorence@gmail.com>
2020-09-26 00:04:09 -0600

    better seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -81,8 +81,21 @@ class DatabaseSeeder extends Seeder
 		$user_ids = [];
 		$pronouns = ['She/Her', 'He/Him', 'They/Them'];
 		$can_take_orders = [true, false];
+		$levels = [
+			'Dad',
+			'Working Mum',
+			'Chef',
+			'Hobbyist',
+			'professional bartender @ macys',
+			'farmer',
+			'Just a Girl',
+			'Cook',
+			'Care giver'
+		];
 
 		for ($j=0; $j<20; $j++) {
+			$rand_level = array_rand($levels);
+
 			$user = new \App\User([
 				'name' => $faker->firstName . ' ' . $faker->lastName,
 				'email' => $faker->email,
@@ -91,7 +104,7 @@ class DatabaseSeeder extends Seeder
 				'following' => 0,
 				'avatar' => $avatars[$j],
 				'pronouns' => $pronouns[rand(0,2)],
-				'expertise_level' => 'professional bartender @ macys',
+				'expertise_level' => $levels[$rand_level],
 				'can_take_orders' => array_rand($can_take_orders),
 				'about' => 'Rrow itself, let it be sorrow; let him love it; let him pursue it, ishing for its acquisitiendum. Because he will ab hold, uniess but through concer, and also of those who resist. Now a pure snore disturbeded sum dust. He ejjnoyes, in order that somewon, also with a severe one, unless of life. May a cusstums offficer somewon nothing of a poison-filled. Until, from a twho, twho chaffinch may also pursue it, not even a lump. But as twho, as a tank; a proverb, yeast; or else they tinscribe nor. Yet yet dewlap bed. Twho may be, let him love fellows of a polecat. Now amour, the, twhose being, drunk, yet twhitch and, an enclosed valley’s always a laugh. In acquisitiendum the Furies are Earth; in (he takes up) a lump vehicles bien',
 				'email_verified' => '2020-01-01 00:00:00'
