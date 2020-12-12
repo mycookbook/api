3b65cb6b81026e5c011fb8b9f670524b2803acb4
fokosun <okosunuzflorence@gmail.com>
2020-12-11 17:01:46 -0700

    Implement custom exception to handle unauthorized client requests

---
+++ app/Exceptions/UnauthorizedClientException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Http\Response;
+
+class UnauthorizedClientException extends \Exception
+{
+	public function report():void {}
+
+	public function render()
+	{
+		return response()->json([
+			'error' => 'Unauthorized'
+		], Response::HTTP_UNAUTHORIZED);
+	}
+}
