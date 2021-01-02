66704d566bfcb01bb4b121f68eafe6c95acdbd0c
fokosun <okosunuzflorence@gmail.com>
2021-01-01 22:26:22 -0700

    encrypt the payload to be sent to the notifications service

---
+++ app/Traits/EncryptsPayload.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Crypt;
+
+trait EncryptsPayload
+{
+	/**
+	 * Encrypts the given payload using Crypt
+	 *
+	 * @param array $payload
+	 * @return string
+	 */
+	public function payload(Array $payload): string
+	{
+		$payload['secret'] = env('CRYPT_SECRET');
+
+		return Crypt::encrypt($payload);
+	}
+}
