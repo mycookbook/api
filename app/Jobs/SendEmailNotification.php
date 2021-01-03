63fc205bb97ad8461e5f61a5c94f864c740978d0
fokosun <okosunuzflorence@gmail.com>
2021-01-02 22:00:57 -0700

    rename class

---
+++ app/Jobs/SendEmailNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Jobs;
+
+class SendEmailNotification extends BaseNotification
+{
+	const TYPE = "email";
+
+	/**
+	 * SendEmailNotification constructor.
+	 *
+	 * @param $userId
+	 * @param string $event
+	 */
+	public function __construct($userId, $event = 'new-user')
+	{
+		parent::__construct($userId);
+
+		$this->payload['type'] = self::TYPE;
+		$this->payload['event'] = $event;
+		$this->payload['email'] = $this->user->email;
+		$this->payload['username'] = $this->user->name;
+	}
+}
