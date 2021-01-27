aa615936d8e4261450b8dcb8ff344a4c4d65afe9
fokosun <okosunuzflorence@gmail.com>
2021-01-27 15:31:30 -0700

    refactor: create adapters and adhere to common interface

--- app/Adapters/Search/AlgoliaAdapter.php
+++ app/Adapters/Search/AlgoliaAdapter.php
@@ -1,10 +1,18 @@
 <?php
 
-
 namespace App\Adapters\Search;
 
+use Illuminate\Support\Collection;
 
-class AlgoliaAdapter
+class AlgoliaAdapter implements FulltextSearchAdapterInterface
 {
 
+	/**
+	 * @param string $q
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function fetch(string $q): \Illuminate\Support\Collection
+	{
+		return new Collection();
+	}
 }
