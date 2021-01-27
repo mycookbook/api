cc4ee5d39f6efa2bff0d4c5a4bdc2b012df9bf49
fokosun <okosunuzflorence@gmail.com>
2021-01-27 15:32:25 -0700

    Refactor

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,10 +8,25 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController implements FormRequest
 {
+	/**
+	 * @var
+	 */
 	protected $service;
+
+	/**
+	 * @var array
+	 */
 	protected $params;
+
+	/**
+	 * @var Request
+	 */
 	public $request;
 
+	/**
+	 * Controller constructor.
+	 * @param Request $request
+	 */
 	public function __construct(Request $request)
 	{
 		$this->params = $request->all();
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Requests\SearchRequest;
+use App\Adapters\Search\FulltextSearchAdapterInterface;
 
 class SearchController extends Controller
 {
+	/**
+	 * @var FulltextSearchAdapterInterface
+	 */
+	protected $adapter;
+
+	/**
+	 * SearchController constructor.
+	 *
+	 * @param Request $request
+	 * @param FulltextSearchAdapterInterface $adapter
+	 */
+	public function __construct(Request $request, FulltextSearchAdapterInterface $adapter)
+	{
+		parent::__construct($request);
+		$this->adapter = $adapter;
+	}
+
 	/**
 	 * @param SearchRequest $request
 	 * @return \Illuminate\Http\JsonResponse
@@ -16,110 +33,8 @@ class SearchController extends Controller
 	{
 		$q = $request->getParams()->input('query');
 
-		$by_cookbook = 'cookbooks by';
-		$by_recipe = 'recipes by';
-
-		if (strpos($q, $by_cookbook) !== false) {
-			$q = trim(str_replace($by_cookbook, "", $q));
-
-			return response()->json([
-				'response' => $this->fetchCookbooks($q)
-			]);
-		}
-
-		if (strpos($q, $by_recipe) !== false) {
-			$q = trim(str_replace($by_recipe, "", $q));
-
-			return response()->json([
-				'response' => $this->fetchRecipes($q)
-			]);
-		}
-
 		return response()->json([
-			'response' => $this->fetchCookbooks($q)->merge($this->fetchRecipes($q))
+			'response' => $this->adapter->fetch($q)
 		]);
-	}
-
-	/**
-	 * @param $q
-	 * @return \Illuminate\Support\Collection
-	 */
-	private function fetchCookbooks($q): \Illuminate\Support\Collection
-	{
-		$query = DB::table('cookbooks')
-			->select([
-				'cookbooks.id AS cookbook_id',
-				'cookbooks.name AS cookbook_name',
-				'cookbooks.slug AS cookbook_slug',
-				DB::raw('SUBSTR(cookbooks.description,1,250) as description'),
-				DB::raw('DATE_FORMAT(cookbooks.created_at, "%d %M %Y") as created_at'),
-				'cookbooks.bookCoverImg',
-				'cookbooks.resource_type',
-				'cookbooks.is_locked',
-				'users.name AS author_name',
-				'users.name_slug AS username',
-				'users.id AS author_id'
-			])
-			->leftJoin('users', 'users.id', '=', 'cookbooks.user_id');
-
-		if ($q === "cookbooks") {
-			return $query->get();
-		}
-
-		return $query
-			->whereRaw("MATCH(cookbooks.name,cookbooks.description) AGAINST(? IN BOOLEAN MODE)", array($q))
-			->orWhereRaw("MATCH(users.name) AGAINST(? IN BOOLEAN MODE)", array($q))
-			->get();
-	}
-
-	/**
-	 * @param $q
-	 * @return \Illuminate\Support\Collection
-	 */
-	private function fetchRecipes($q): \Illuminate\Support\Collection
-	{
-		return DB::table('recipes')
-			->select([
-				'recipes.id as recipe_id',
-				'recipes.name AS recipe_name',
-				'recipes.slug AS recipe_slug',
-				DB::raw('SUBSTR(recipes.summary,1,250) as summary'),
-				'recipes.ingredients',
-				'recipes.resource_type',
-				'recipes.nutritional_detail',
-				'recipes.imgUrl',
-				'recipes.cookbook_id AS cookbook_id',
-				DB::raw('DATE_FORMAT(recipes.created_at, "%d %M %Y") as created_at'),
-				'users.name AS author_name',
-				'users.name_slug AS username',
-				'users.id AS author_id'
-			])
-			->leftJoin('users', 'users.id', '=', 'recipes.user_id')
-			->whereRaw("MATCH(recipes.name,recipes.description,recipes.ingredients,recipes.nutritional_detail,recipes.summary) AGAINST(? IN BOOLEAN MODE)", array($q))
-			->orWhereRaw("MATCH(users.name) AGAINST(? IN BOOLEAN MODE)", array($q))
-			->get();
-	}
-
-	/**
-	 * Get the user meta data and write to a csv file for ML purposes
-	 *
-	 * @param Request $request
-	 */
-	public function writeToCsv(Request $request)
-	{
-		$csv = $request->only([
-			'city',
-			'country',
-			'ip',
-			'keyword',
-			'loc',
-			'timezone'
-		]);
-
-		$csv['server_time'] = \Carbon\Carbon::now()->toDateTimeString();
-
-		$file_open = fopen('keywords.csv', 'a+');
-
-		fputcsv($file_open, $csv);
 	}
 }
