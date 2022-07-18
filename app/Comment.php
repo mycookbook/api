b01d8e4e965fa68e72a46cd9c41f4eb03bfa62bb
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-07-18 15:39:33 -0400

    wip

--- app/Comment.php
+++ app/Comment.php
@@ -9,4 +9,16 @@ class Comment extends Model
     protected $fillable = [
         'user_id', 'recipe_id', 'comment'
     ];
+
+    protected $appends = ['author'];
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorAttribute()
+    {
+        $author_id = $this->user_id;
+
+        return User::where(["id" => $author_id])->first();
+    }
 }
