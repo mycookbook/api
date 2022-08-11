ef0bd18a41f2e3f7fe51e6a88300b5bd703b80c9
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-11 01:32:36 -0400

    chore fixing th app after swapping lumen with laravel

--- app/Services/CookbookService.php
+++ app/Services/CookbookService.php
@@ -40,8 +40,8 @@ class CookbookService implements serviceInterface
         return response()->json(
             [
                 'data' => $cookbooks->take(15)->get()
-                    ->orderByDesc('created_at')
-                    ->get(),
+//                    ->orderByDesc('created_at')
+//                    ->get(),
             ], Response::HTTP_OK
         );
     }
