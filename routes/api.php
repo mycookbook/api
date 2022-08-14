dbf3e60eebc894dca47a43ab78a8a99e824b778e
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-14 17:01:57 -0400

    give the api routes file a tiny taylor otwell facelift

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CookbookController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,55 +11,23 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-
-Route::get('/v1/users/{id}/verify', function ($id) {
-    $user = \App\Models\User::where(['id' => $id])->orWhere(['email' => $id])->get()->first();
-
-    $user->update([
-        'email_verified' => \Carbon\Carbon::now(),
-    ]);
-
-    return $user;
-});
-
-Route::get('v1/create-auth-client', function () {
-    $api_key = \Illuminate\Support\Facades\Crypt::encryptString('Hello DevDojo');
-
-    $client = new \App\Models\AuthorizedClient([
-        'api_key' => $api_key,
-        'client_secret' => 'Hello DevDojo',
-        'passphrase' => 'potatoes',
-    ]);
-
-    if ($client->save()) {
-        return $client;
-    }
-
-    return null;
-});
 
 Route::group(['prefix' => 'v1'], function () {
 
+    /*
+    |--------------------------------------------------------------------------
+    | Auth group
+    |--------------------------------------------------------------------------
+    |
+    */
     Route::prefix('/auth')->group(function() {
 
-        Route::post(
-            '/register', 'UserController@store'
-        );
+        Route::post('/register', [UserController::class, 'store']);
 
-        Route::post(
-            '/login', [
-                \App\Http\Controllers\AuthController::class, 'login'
-            ]
-        );
+        Route::post('/login', [AuthController::class, 'login']);
 
-        Route::get(
-            '/logout', 'AuthController@logout'
-        );
+        Route::get('/logout', [AuthController::class, 'logout']);
 
         //tiktok
         Route::get('/tiktok', [
@@ -104,116 +74,71 @@ Route::group(['prefix' => 'v1'], function () {
         ]);
     });
 
-    Route::get(
-        '/tiktok', 'AuthController@tikTokHandleCallback'
-    );
+    /*
+    |--------------------------------------------------------------------------
+    | Users group
+    |--------------------------------------------------------------------------
+    |
+    */
+    Route::group(['prefix' => '/users'], function () {
 
-    Route::get('/definitions', 'DefinitionsController@index');
-    Route::get(
-        '/policies', 'StaticContentController@get'
-    );
+        Route::get('/', [UserController::class, 'index']);
 
-    Route::get(
-        '/users/', 'UserController@index'
-    );
+        Route::get('/{username}', [UserController::class, 'show']);
 
-    Route::get(
-        '/users/{username}', 'UserController@show'
-    );
-
-    Route::get(
-        '/search', 'SearchController@fetch'
-    );
-
-    Route::post(
-        '/keywords', 'SearchController@writeToCsv'
-    );
-
-    Route::group([
-        'middleware' => [
-            'throttle',
-        ], ], function () {
-        Route::get('flags', function () {
-            return response()->json([
-                'data' => \App\Models\Flag::all(),
-            ]);
-        });
-
-        Route::get('categories', function () {
-            return response()->json([
-                'data' => \App\Models\Category::all(),
-            ]);
-        });
-
-        /*
-        |--------------------------------------------------------------------------
-        | Statistics
-        |--------------------------------------------------------------------------
-        */
-        Route::get(
-            '/stats/', 'StatsController@index'
-        );
-
-        /**
-         * Email verification
-         */
-        Route::get('verify-email/{token}', 'UserController@verifyEmail');
-        Route::get('resend-email-verification-link/{token}', 'UserController@resend');
-
-        /*
-        |--------------------------------------------------------------------------
-        | PROTECTED ROUTES
-        |--------------------------------------------------------------------------
-        */
-        Route::group(
-            [
-                'middleware' => [
-                    'throttle',
-                ],
-            ], function () {
-            Route::post(
-                '/users/{username}', 'UserController@update'
-            );
-
-            Route::patch(
-                '/users/{username}', 'UserController@update'
-            );
-
-            /*
-            |--------------------------------------------------------------------------
-            | Recipes Routes
-            |--------------------------------------------------------------------------
-            */
-
-            Route::get('/recipes', [RecipeController::class, 'index']);
-            Route::get('/recipes/{id}', [RecipeController::class, 'show']);
-            Route::get('/my/recipes', [RecipeController::class, 'myRecipes']);
-
-            Route::post('/recipes', [RecipeController::class, 'store']);
-            Route::post('/recipes/{id}/edit', [RecipeController::class, 'update']);
-            Route::post('/recipes/{id}/destroy', [RecipeController::class, 'destroy']);
-
-            /*
-            |--------------------------------------------------------------------------
-            | Cookbooks Routes
-            |--------------------------------------------------------------------------
-            */
-
-            Route::get('/cookbooks', [CookbookController::class, 'index']);
-            Route::get('/cookbooks/{id}', [CookbookController::class, 'show']);
-            Route::get('/my/cookbooks', [CookbookController::class, 'myCookbooks']);
-
-            Route::post('/cookbooks', [CookbookController::class, 'store']);
-            Route::post('/cookbooks/{id}/edit', [CookbookController::class, 'update']);
-            Route::post('/cookbooks/{id}/destroy', [CookbookController::class, 'destroy']);
-        });
+        Route::post('/{username}/edit', [UserController::class, 'update']);
     });
 
     /*
-     * |--------------------------------------------------------------------------
-     * | Subscriptions
-     * |--------------------------------------------------------------------------
-   */
+    |--------------------------------------------------------------------------
+    | Recipes group
+    |--------------------------------------------------------------------------
+    |
+    */
+    Route::group(['prefix' => '/recipes'], function () {
+        Route::get('/', [RecipeController::class, 'index']);
+        Route::get('/{id}', [RecipeController::class, 'show']);
+
+        Route::post('/', [RecipeController::class, 'store']);
+        Route::post('/{id}/edit', [RecipeController::class, 'update']);
+        Route::post('/{id}/destroy', [RecipeController::class, 'destroy']);
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cookbooks group
+    |--------------------------------------------------------------------------
+    |
+    */
+    Route::group(['prefix' => '/cookbooks'], function () {
+        Route::get('/', [CookbookController::class, 'index']);
+        Route::get('//{id}', [CookbookController::class, 'show']);
+
+        Route::post('/', [CookbookController::class, 'store']);
+        Route::post('/{id}/edit', [CookbookController::class, 'update']);
+        Route::post('/{id}/destroy', [CookbookController::class, 'destroy']);
+    });
+
+    Route::get('/tiktok', 'AuthController@tikTokHandleCallback');
+
+    Route::get('/definitions', 'DefinitionsController@index');
+
+    Route::get('/policies', 'StaticContentController@get');
+
+    Route::get('/search', 'SearchController@fetch');
+
+    Route::post('/keywords', 'SearchController@writeToCsv');
+
+    Route::get('/my/recipes', [RecipeController::class, 'myRecipes']);
+
+    Route::get('/my/cookbooks', [CookbookController::class, 'myCookbooks']);
+
+    Route::get('verify-email/{token}', 'UserController@verifyEmail');
+
+    Route::get('resend-email-verification-link/{token}', 'UserController@resend');
+
+    Route::get('/stats/', 'StatsController@index');
+
     Route::post('subscriptions', 'SubscriptionController@store');
 
     Route::get('/categories', 'CategoryController@index');
