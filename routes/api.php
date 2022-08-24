69d26b4f261d3d197cdeca83f4c45213875b0da7
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-23 23:23:42 -0400

    wip tiktok login

--- routes/api.php
+++ routes/api.php
@@ -28,50 +28,6 @@ Route::group(['prefix' => 'v1'], function () {
         Route::post('/login', [AuthController::class, 'login']);
 
         Route::get('/logout', [AuthController::class, 'logout']);
-
-        //tiktok
-        Route::get('/tiktok', [
-            'uses' => 'AuthController@socialAuth',
-            'provider' => 'tiktok',
-        ]);
-
-        Route::get('/callback/tiktok', [
-            'uses' => 'AuthController@socialAuthCallbackHandler',
-            'provider' => 'tiktok',
-        ]);
-
-        //twitter
-        Route::get('/twitter', [
-            'uses' => 'AuthController@socialAuth',
-            'provider' => 'twitter',
-        ]);
-
-        Route::get('/callback/twitter', [
-            'uses' => 'AuthController@socialAuthCallbackHandler',
-            'provider' => 'twitter',
-        ]);
-
-        //pinterest
-        Route::get('/pinterest', [
-            'uses' => 'AuthController@socialAuth',
-            'provider' => 'pinterest',
-        ]);
-
-        Route::get('/callback/pinterest', [
-            'uses' => 'AuthController@socialAuthCallbackHandler',
-            'provider' => 'pinterest',
-        ]);
-
-        //instagram
-        Route::get('/instagram', [
-            'uses' => 'AuthController@socialAuth',
-            'provider' => 'instagram',
-        ]);
-
-        Route::get('/callback/instagram', [
-            'uses' => 'AuthController@socialAuthCallbackHandler',
-            'provider' => 'instagram',
-        ]);
     });
 
     /*
