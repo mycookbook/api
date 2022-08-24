f1034603d605efa1ea4c881c80eb3fa1cbfb363d
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-23 23:23:42 -0400

    wip tiktok login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Cookbooks api v1';
 });
+
+//tiktok
+Route::get('/tiktok', [
+    'uses' => 'AuthController@socialAuth',
+    'provider' => 'tiktok',
+]);
+
+Route::get('/callback/tiktok', [
+    'uses' => 'AuthController@socialAuthCallbackHandler',
+    'provider' => 'tiktok',
+]);
+
+//twitter
+Route::get('/twitter', [
+    'uses' => 'AuthController@socialAuth',
+    'provider' => 'twitter',
+]);
+
+Route::get('/callback/twitter', [
+    'uses' => 'AuthController@socialAuthCallbackHandler',
+    'provider' => 'twitter',
+]);
+
+//pinterest
+Route::get('/pinterest', [
+    'uses' => 'AuthController@socialAuth',
+    'provider' => 'pinterest',
+]);
+
+Route::get('/callback/pinterest', [
+    'uses' => 'AuthController@socialAuthCallbackHandler',
+    'provider' => 'pinterest',
+]);
+
+//instagram
+Route::get('/instagram', [
+    'uses' => 'AuthController@socialAuth',
+    'provider' => 'instagram',
+]);
+
+Route::get('/callback/instagram', [
+    'uses' => 'AuthController@socialAuthCallbackHandler',
+    'provider' => 'instagram',
+]);
+
+Route::get('/webhooks/tiktok', function() {
+    return response()->json([
+        'message' => 'payload recieved with thanks'
+    ]);
+});
