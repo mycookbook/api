935a032e59e5efc47c4c55fac98a73dcb9c4c0df
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-28 11:58:45 -0400

    fixing form request validations

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Adapters\Search\FulltextSearchAdapterInterface;
 use App\Http\Requests\SearchRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SearchController extends Controller
 {
@@ -24,8 +27,18 @@ class SearchController extends Controller
      * @param  SearchRequest  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getSearchResults(SearchRequest $request): \Illuminate\Http\JsonResponse
+    public function getSearchResults(Request $request): \Illuminate\Http\JsonResponse
     {
+        $validator = Validator::make($request->all(), [
+            'query' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 411);
+        }
+
         return response()->json([
             'response' => $this->adapter->fetch($request->get('query')),
         ]);
