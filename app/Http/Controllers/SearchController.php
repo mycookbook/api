062b280813b585c2db60a473bfd5510a0b4cc1fc
System Administrator <root@Obiorass-MacBook-Pro.local>
2022-08-27 21:39:43 -0400

    fixing search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,25 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Adapters\Search\FulltextSearchAdapterInterface;
-use App\Http\Controllers\Requests\SearchRequest;
-use Illuminate\Http\Request;
+use App\Http\Requests\SearchRequest;
 
 class SearchController extends Controller
 {
     /**
      * @var FulltextSearchAdapterInterface
      */
-    protected $adapter;
+    protected FulltextSearchAdapterInterface $adapter;
 
     /**
-     * SearchController constructor.
-     *
-     * @param  Request  $request
      * @param  FulltextSearchAdapterInterface  $adapter
      */
-    public function __construct(Request $request, FulltextSearchAdapterInterface $adapter)
+    public function __construct(FulltextSearchAdapterInterface $adapter)
     {
-        parent::__construct($request);
         $this->adapter = $adapter;
     }
 
@@ -29,12 +24,10 @@ class SearchController extends Controller
      * @param  SearchRequest  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function fetch(SearchRequest $request): \Illuminate\Http\JsonResponse
+    public function getSearchResults(SearchRequest $request): \Illuminate\Http\JsonResponse
     {
-        $q = $request->getParams()->input('query');
-
         return response()->json([
-            'response' => $this->adapter->fetch($q),
+            'response' => $this->adapter->fetch($request->get('query')),
         ]);
     }
 }
