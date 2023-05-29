1f023e981ce7ae899c308c303c8e44887afacc23
fokosun <okosunuzflorence@gmail.com>
2023-05-29 15:52:17 -0400

    enable Access-Control-Allow-Headers

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -13,6 +13,7 @@ class HandleCors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Headers', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
