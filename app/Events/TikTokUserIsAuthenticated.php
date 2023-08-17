5bf25acba008a25a9aef7fff1723f9082746c2cd
fokosun <okosunuzflorence@gmail.com>
2023-08-17 12:33:28 -0400

    debugging tiktok

--- app/Events/TikTokUserIsAuthenticated.php
+++ app/Events/TikTokUserIsAuthenticated.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Events;
 
 use App\Dtos\TikTokUserDto;
+use App\Models\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -13,10 +14,17 @@ class TikTokUserIsAuthenticated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    protected User $user;
+
     /**
      * Create a new event instance.
      */
     public function __construct(
         public TikTokUserDto $tikTokUserDto
     ) {}
+
+    public function getUser()
+    {
+        return User::findOrFail($this->tikTokUserDto->getUserId());
+    }
 }
