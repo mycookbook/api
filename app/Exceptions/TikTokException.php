411757607e879145603a685fdb42b2ed3b342a68
fokosun <okosunuzflorence@gmail.com>
2023-08-17 16:09:07 -0400

    debugging tt

--- app/Exceptions/TikTokException.php
+++ app/Exceptions/TikTokException.php
@@ -6,8 +6,9 @@ use Exception;
 
 class TikTokException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = "", array $context = [])
     {
+        $message = $message . json_encode($context);
         parent::__construct($message, $code, $previous);
     }
 }
