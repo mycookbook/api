f0dce08926ed336960526c19c1bb5b4dcaaef7c9
fokosun <okosunuzflorence@gmail.com>
2023-08-20 22:33:43 -0400

    debug tiktok again

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -155,69 +155,68 @@ class AuthController extends Controller
             );
 
             $decoded = json_decode($response->getBody()->getContents(), true);
-            dd($decoded);
 
-//            if ($decoded['message'] === 'error') {
-//                throw new \Exception(json_encode($decoded));
-//            }
-//
-//            $userInfoResponse = $client->request('POST',
-//                'https://open-api.tiktok.com/user/info/',
-//                [
-//                    'json' => [
-//                        'open_id' => $decoded['data']['open_id'],
-//                        'access_token' => $decoded['data']['access_token'],
-//                        'fields' => [
-//                            'open_id',
-//                            'avatar_url',
-//                            'display_name',
-//                            'avatar_url_100',
-//                            'is_verified',
-//                            'profile_deep_link',
-//                            'bio_description',
-//                            'display_name',
-//                            'avatar_large_url',
-//                            'avatar_url_100',
-//                            'union_id',
-//                            'video_count'
-//                        ],
-//                    ],
-//                ]
-//            );
-//
-//            $userInfo = json_decode($userInfoResponse->getBody()->getContents(), true);
-//
-//            if (!empty($userInfo['data']['user'])) {
-//                $tiktokEmail = $userInfo['data']['user']['open_id'] . '@tiktok.com';
-//
-//                $user = User::where(['email' => $tiktokEmail])->first();
-//
-//                if (!$user instanceof User) {
-//                    $response = $service->store(new Request([
-//                        'name' => $userInfo['data']['user']['display_name'],
-//                        'email' => $tiktokEmail,
-//                        'password' => 'fakePass',
-//                    ]));
-//
-//                    $decoded = json_decode($response->getContent(), true);
-//                    $data = $decoded['response']['data'];
-//                    $user = User::where(['email' => $data['email']])->first();
-//                }
-//
-//                $user->update([
-//                    'avatar' => $userInfo['data']['user']['avatar_url'],
-//                    'pronouns' => 'They/Them',
-//                ]);
-//
-//                $credentials = [
-//                    'email' => $user->email,
-//                    'password' => 'fakePass',
-//                ];
-//
-//                if (!$token = Auth::attempt($credentials)) {
-//                    return redirect('https://web.cookbookshq.com/#/errors/?m=there was an error processing this request, please try again.');
-//                }
-//
+            if ($decoded['message'] === 'error') {
+                throw new \Exception(json_encode($decoded));
+            }
+
+            $userInfoResponse = $client->request('POST',
+                'https://open-api.tiktok.com/user/info/',
+                [
+                    'json' => [
+                        'open_id' => $decoded['data']['open_id'],
+                        'access_token' => $decoded['data']['access_token'],
+                        'fields' => [
+                            'open_id',
+                            'avatar_url',
+                            'display_name',
+                            'avatar_url_100',
+                            'is_verified',
+                            'profile_deep_link',
+                            'bio_description',
+                            'display_name',
+                            'avatar_large_url',
+                            'avatar_url_100',
+                            'union_id',
+                            'video_count'
+                        ],
+                    ],
+                ]
+            );
+
+            $userInfo = json_decode($userInfoResponse->getBody()->getContents(), true);
+
+            if (!empty($userInfo['data']['user'])) {
+                $tiktokEmail = $userInfo['data']['user']['open_id'] . '@tiktok.com';
+
+                $user = User::where(['email' => $tiktokEmail])->first();
+
+                if (!$user instanceof User) {
+                    $response = $service->store(new Request([
+                        'name' => $userInfo['data']['user']['display_name'],
+                        'email' => $tiktokEmail,
+                        'password' => 'fakePass',
+                    ]));
+
+                    $decoded = json_decode($response->getContent(), true);
+                    $data = $decoded['response']['data'];
+                    $user = User::where(['email' => $data['email']])->first();
+                }
+
+                $user->update([
+                    'avatar' => $userInfo['data']['user']['avatar_url'],
+                    'pronouns' => 'They/Them',
+                ]);
+
+                $credentials = [
+                    'email' => $user->email,
+                    'password' => 'fakePass',
+                ];
+
+                if (!$token = Auth::attempt($credentials)) {
+                    return redirect('https://web.cookbookshq.com/#/errors/?m=there was an error processing this request, please try again.');
+                }
+
 //                TikTokUserIsAuthenticated::dispatch(new TikTokUserDto(
 //                    $user->getKey(),
 //                    $userInfo['data']['user']['open_id'],
@@ -232,16 +231,16 @@ class AuthController extends Controller
 //                    $userInfo['data']['user']['union_id'],
 //                    $userInfo['data']['user']['video_count']
 //                ));
-//
-//                $to = 'https://web.cookbookshq.com/#/tiktok/?' . http_build_query([
-//                        'token' => $token,
-//                        '_d' => $user->getSlug(),
-//                    ]);
-//
-//                return redirect($to);
-//            } else {
-//                return redirect('https://web.cookbookshq.com/#/errors/?m=Hey, it looks like your tiktok account is Private. Please login using a public account.');
-//            }
+
+                $to = 'https://web.cookbookshq.com/#/tiktok/?' . http_build_query([
+                        'token' => $token,
+                        '_d' => $user->getSlug(),
+                    ]);
+
+                return redirect($to);
+            } else {
+                return redirect('https://web.cookbookshq.com/#/errors/?m=Hey, it looks like your tiktok account is Private. Please login using a public account.');
+            }
         } catch (\Exception $e) {
             Log::debug('There was an error', [
                 'error' => $e->getMessage(),
