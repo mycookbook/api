d216a9b43209b2e6a5f190a55a0b1b308a38ebfd
fokosun <okosunuzflorence@gmail.com>
2023-08-21 20:29:50 -0400

    fix static analysis errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Adapters\Search\FulltextSearchAdapterInterface;
-use App\Adapters\Search\MySqlAdapter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind(FulltextSearchAdapterInterface::class, MySqlAdapter::class);
+        //
     }
 }
